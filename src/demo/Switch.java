ee017c2b9bc3da702c3d827d0b5e52cfceee988b
fouad <fsidisaid@gmail.com>
2020-04-21 14:07:53 -0400

    this is just training

---
+++ src/demo/Switch.java
@@ -0,0 +1,43 @@
+package demo;
+
+public class Switch {
+
+	
+	public static void main(String[] args) {
+		
+		int studentscore=90;
+		
+		switch(studentscore) {
+		
+	
+		case 90:
+		System.out.println("excellent student");
+		break;
+		case 70:
+			System.out.println(" very good student");
+		break;
+		case 50:
+			System.out.println("good student");
+		break;
+		
+		case 45:
+			System.out.println(" student needs to make more effort");
+			break;
+			default:
+				System.out.println(" socre is not defined");
+		break;
+		}
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+}
